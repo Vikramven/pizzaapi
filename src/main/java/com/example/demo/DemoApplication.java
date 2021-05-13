9b0b8f38566d43291f262e06e97c70e39f3b29a3
Vikramven <68946012+Vikramven@users.noreply.github.com>
2021-05-13 18:43:25 +0200

    Add files via upload

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+//so basically this restcontroller makes us serve the endpoints and the only one we have here is the getMapping string hello thingy
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	public List<String> hello(){
+		return List.of("Hello", "World");
+	}
+}
